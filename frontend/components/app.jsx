053e83a5ab5b49cf631de52770d7f479b3254760
randyjap <randyjap@gmail.com>
2017-03-01 19:19:35 -0800

    Prepare layout

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import HeaderContainer from './header/header_container';
+import FooterContainer from './footer/footer_container';
 
 const App = ({ children }) => (
   <div className="wrapper">
+    <HeaderContainer />
     { children }
+    <FooterContainer />
   </div>
 );
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router';
+import TextField from 'material-ui/TextField';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TimeAgo from 'react-timeago';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+class Footer extends React.Component{
+  constructor(props){
+    super(props);
+    this.redirect = this.redirect.bind(this);
+  }
+
+  redirect(route){
+    this.props.router.replace(route);
+  }
+
+  render(){
+    return (
+      <footer className="footer">
+        Footer
+      </footer>
+    );
+  }
+}
+
+export default Footer;
