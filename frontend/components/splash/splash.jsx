2279ac03f97ec24d7de0c03c8f0564bb9b4d9759
randyjap <randyjap@gmail.com>
2017-03-07 16:25:22 -0800

    Add secondary tiles to main page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -45,6 +45,58 @@ class Splash extends React.Component{
             <img src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488928247/example_pkzok0.png" />
           </div>
         </Paper>
+        <div className="two-column">
+          <div className="article-tile">
+            <Paper zDepth={1} className="carousel-tile">
+              <Carousel>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+              </Carousel>
+              <div className="main-carousel-bottom">
+                <div className="article">
+                  <p className="article-title">Si Chuan Restaurant</p>
+                  <p className="article-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+              </div>
+            </Paper>
+          </div>
+          <div className="article-tile">
+            <Paper zDepth={1} className="carousel-tile">
+              <Carousel>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+                <img onClick={() => console.log("click")} className="carousel-image" src="http://res.cloudinary.com/dkympkwdz/image/upload/v1488929343/screenshot_hctzhq.jpg"/>
+              </Carousel>
+              <div className="main-carousel-bottom">
+                <div className="article">
+                  <p className="article-title">Si Chuan Restaurant</p>
+                  <p className="article-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+              </div>
+            </Paper>
+          </div>
+        </div>
       </div>
     );
   }
