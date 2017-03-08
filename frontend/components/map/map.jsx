9c81b8eea5c93a9da462dce223d907681f2eaacc
randyjap <randyjap@gmail.com>
2017-03-07 16:55:18 -0800

    Add css to restaurant page

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -116,9 +116,10 @@ class Map extends Component {
 
   render(){
     return (
-      <div>
+      <div className="map-container">
         <div className="map" id="map-container" ref="map">Map</div>
-        <div id="change-me">HELLO! Click the markers above!</div>
+        <div>HELLO! Click the markers above!</div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     );
   }
