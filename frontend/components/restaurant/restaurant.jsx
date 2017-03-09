9554e649ecec41102e5e5769b62bb91a4e62ad30
randyjap <randyjap@gmail.com>
2017-03-08 16:06:01 -0800

    Restyled restaurant page

--- frontend/components/restaurant/restaurant.jsx
+++ frontend/components/restaurant/restaurant.jsx
@@ -9,8 +9,23 @@ class Restaurant extends React.Component{
 
   render(){
     return (
-      <div className="restaurant">
-        <MapContainer />
+      <div className="main">
+        <div className="main-header">
+          <ul className="main-header-nav">
+            <li><a href="#">Restaurants</a></li>
+            <li><a href="#">Guides</a></li>
+            <li><a href="#">Style</a></li>
+            <li><a href="#">Galleries</a></li>
+            <li><a href="#">Lifestyle</a></li>
+            <li><a href="#">Videos</a></li>
+          </ul>
+        </div>
+        <div className="main-body">
+          <MapContainer />
+        </div>
+        <footer className="footer">
+
+        </footer>
       </div>
     );
   }
