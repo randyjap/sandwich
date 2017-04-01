52d67881493f5f1c4c5850bd6507797ac3e0e9f9
randyjap <randyjap@gmail.com>
2017-03-31 17:53:21 -0700

    add lat, lng, category

--- db/migrate/20170303050046_create_articles.rb
+++ db/migrate/20170303050046_create_articles.rb
@@ -6,7 +6,11 @@ class CreateArticles < ActiveRecord::Migration[5.0]
       t.text :article, null: false
       t.date :date, null: false
       t.integer :views, default: 0
+      t.string :category, null: false
+      t.float :lat, null: false
+      t.float :lng, null: false
       t.timestamps
     end
+    add_index :articles, :author_id
   end
 end
